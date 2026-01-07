2c40f762a27fc60b1d323fe0af84588468efdb22
CyberNinja9 <mirzayusifmirzayevacc@gmail.com>
2026-01-07 12:50:28 +0100

    db.php

--- db.php
+++ db.php
@@ -1,16 +1,16 @@
 <?php
-$databaseUrl = getenv("DATABASE_URL");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-if (!$databaseUrl) {
-    die("DATABASE_URL not set");
-}
+$databaseUrl = getenv("DATABASE_URL");
+var_dump($databaseUrl);
 
 try {
-    $pdo = new PDO($databaseUrl, null, null, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]);
+    $pdo = new PDO($databaseUrl);
+    echo "DB CONNECTED OK";
 } catch (Exception $e) {
-    die("DB connection failed");
+    echo "<pre>";
+    echo $e->getMessage();
+    echo "</pre>";
 }
-
