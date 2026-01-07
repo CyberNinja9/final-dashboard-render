3b2d6bdfcda91bada012b293fbc31bc6bdb4db1c
CyberNinja9 <mirzayusifmirzayevacc@gmail.com>
2026-01-07 10:30:46 +0100

    Add files via upload

---
+++ final_exam_5/products_by_category.php
@@ -0,0 +1,40 @@
+<?php
+header("Content-Type: application/json");
+
+$host = "fdb1034.awardspace.net";
+$dbname = "4706409_mirza68843";
+$user = "4706409_mirza68843";
+$pass = "Acca2023";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+
+if ($conn->connect_error) {
+    http_response_code(500);
+    echo json_encode(["error" => "Database connection failed"]);
+    exit;
+}
+
+
+$sql = "
+    SELECT category, COUNT(*) AS total
+    FROM products
+    GROUP BY category
+";
+
+$result = $conn->query($sql);
+
+$data = [];
+
+while ($row = $result->fetch_assoc()) {
+    $data[] = [
+        "category" => $row["category"],
+        "total" => (int)$row["total"]
+    ];
+}
+
+$conn->close();
+
+
+echo json_encode($data);
+
