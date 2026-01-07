cb12a56df7487989cb90db1056f84daa73e8579d
CyberNinja9 <mirzayusifmirzayevacc@gmail.com>
2026-01-07 13:04:43 +0100

    db.php

--- db.php
+++ db.php
@@ -1,16 +1,12 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 $databaseUrl = getenv("DATABASE_URL");
-var_dump($databaseUrl);
 
-try {
-    $pdo = new PDO($databaseUrl);
-    echo "DB CONNECTED OK";
-} catch (Exception $e) {
-    echo "<pre>";
-    echo $e->getMessage();
-    echo "</pre>";
-}
+$pdo = new PDO(
+    $databaseUrl . "?sslmode=require",
+    null,
+    null,
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]
+);
